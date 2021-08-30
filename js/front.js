99a7c4631559ee09ae2ca15d40dd37a1b22ee052
nks <nks@ulalalab.com>
2021-08-30 17:55:52 +0900

    ing..

--- js/front.js
+++ js/front.js
@@ -6,7 +6,6 @@ function nav () {
     MAINNAV.find("li").off("mouseenter").on("mouseenter", function() {
         let $this   = $(this),
             index   = $this.index(),
-            target  = $this.children("a"),
             subNav  = SUBNAV.find("ul"),
             sel     = subNav.eq(`${index - 1}`),
             unsel   = subNav.not(`:eq(${index - 1})`);
@@ -38,9 +37,9 @@ function nav () {
 }
 
 function select () {
-    const selectText = $(".select-text");
+    const SELECTTEXT = $(".select-text");
 
-    selectText.off("click.select").on("click.select", function () {
+    SELECTTEXT.off("click.select").on("click.select", function () {
 
         let ul     = $(this).next("ul"),
             method = "slideUp";
@@ -53,7 +52,31 @@ function select () {
     })
 }
 
+function listTypeChange () {
+    const LISTS     = $(".lists-box"),
+          RADIOBTNS = $("input[name='listType']:radio");
+    
+    RADIOBTNS.each(function (index, item) {
+        if(RADIOBTNS[index].checked === true) {
+            LISTS[(RADIOBTNS[index].value === "galleryType") ? "addClass" : "removeClass"]("gallery-type");
+        }
+    })
+
+    RADIOBTNS.change(function () {
+        let value  = this.value,
+            method = "removeClass";
+        
+        if(value === "galleryType") {
+            method = "addClass";
+        }
+
+        LISTS[method]("gallery-type");
+    })
+    
+}
+
 $(document).ready(function () {
     nav();
     select();
+    listTypeChange();
 });
