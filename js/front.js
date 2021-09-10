f7f77d1bfc2a0a4c030aec3303dc39dc38135d33
nks <nks@ulalalab.com>
2021-09-10 15:55:30 +0900

    add pages

--- js/front.js
+++ js/front.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     select();
     listTypeChange();
     modal('a[data-modal]');
+    tab();
+    accordion();
 });
 
 function modal (target) {
@@ -93,4 +95,53 @@ function listTypeChange () {
         LISTS[method]("gallery-type");
     })
     
+}
+
+function tab () {
+    const TABMENU = $("[data-tabMenu]").find("li");
+    const TABBTN  = TABMENU.find("a");
+    const TABCON  = $("[data-tabMenu]").siblings("[data-tabCon]");
+
+    TABMENU.each(function (index, tabmenus) {
+        if ($(tabmenus).find("a").hasClass("on")) {
+            TABCON.each(function (idx, tabs) {
+                $(tabs).data("tabcon") === $(tabmenus).find("a").data("tab") ? $(tabs).addClass("on") : $(tabs).removeClass("on")
+            })
+        }
+    })
+
+    TABBTN.off("click.tabmenu").on("click.tabmenu", function (e) {
+        e.preventDefault()
+        let tabNum = $(this).data("tab");
+        if (tabNum === undefined) {
+            return false;
+        }
+        TABMENU.find("a").removeClass("on");
+        $(this).addClass("on");
+        TABCON.each(function (index, item) {
+            $(item).data("tabcon") === tabNum ? $(item).addClass("on") : $(item).removeClass("on");
+        })
+    })
+}
+
+function accordion () {
+    const ITME = $(".accordion-btn");
+
+    ITME.off("click.accordion").on("click.accordion", function(e) {
+        let target = $(e.currentTarget);
+        let parent = target.closest(".accordion-item");
+        let accCon = $(".accordion-contents");
+
+        if (target.siblings(accCon).length === 0) {
+            return false;
+        }
+
+        if (parent.hasClass("active")) {
+            parent.removeClass("active").find(accCon).slideUp("fast");
+        } else {
+            parent.addClass("active").siblings().removeClass("active");
+            accCon.slideUp("fast");
+            parent.find(accCon).slideDown("fast");
+        }
+    })
 }
